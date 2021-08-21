fe7fc9c5f81f269e0be4ecb2be61a4b7d4e1a06f
Val Nuñez <val@friendlyhello.com>
2021-08-20 20:03:42 -0700

    Added a speedometer and rpm UI
    Also added logic for stopping speedometer and rpm meters from running if car flies off the edge of the world.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
@@ -28,8 +29,23 @@ public class PlayerController : MonoBehaviour
 
     private Rigidbody playerRb;
 
+    [SerializeField] List<WheelCollider> allWheels;
+    [SerializeField] int wheelsOnGround;
+
+    // UI
+    [SerializeField]
+    TMP_Text spedometerText;
 
     
+    public float speed = 0.0f;
+
+    [SerializeField]
+    TMP_Text rpmText;
+
+    public float rpm = 0.0f;
+
+
+
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
@@ -39,7 +55,7 @@ public class PlayerController : MonoBehaviour
     }
 
     
-    void Update()
+    void FixedUpdate()
     {
         // Map axis input to "Horizontal"
         horizontalInput = Input.GetAxis("Horizontal");
@@ -57,12 +73,45 @@ public class PlayerController : MonoBehaviour
         // Turn/Steer the vehicle forward or backward
         //transform.Translate(Vector3.forward * Time.deltaTime * moveSpeed * verticalInput);
 
+        if(IsOnGround())
+        {
+            //** NEW CODE! */
 
-        //** NEW CODE! */
+            playerRb.AddRelativeForce(Vector3.forward * verticalInput * horsePower);
 
-        playerRb.AddRelativeForce(Vector3.forward * verticalInput * horsePower);
+            // Turn/Steer the vehicle with transform.Rotate
+            transform.Rotate(Vector3.up * Time.deltaTime * turnSpeed * horizontalInput);
 
-        // Turn/Steer the vehicle with transform.Rotate
-        transform.Rotate(Vector3.up * Time.deltaTime * turnSpeed * horizontalInput);
+            // UI
+
+            // Speedometer - MPH
+            speed = Mathf.Round(playerRb.velocity.magnitude * 2.237f);
+            spedometerText.SetText("Speed: " + speed + " mph");
+
+            // RPM
+            rpm = Mathf.Round((speed % 30) * 40);
+            rpmText.SetText("RPM: " + rpm);
+        }
+    }
+
+    bool IsOnGround()
+    {
+        wheelsOnGround = 0;
+        foreach (WheelCollider wheel in allWheels)
+        {
+            if (wheel.isGrounded)
+            {
+                wheelsOnGround++;
+            }
+        }
+
+        if (wheelsOnGround == 4)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
 }
